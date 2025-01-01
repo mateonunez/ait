baeef349ff5336d89d6e3fdc5b813f90d173c628
mateonunez <mateonunez95@gmail.com>
2025-01-01 17:41:42 +0100

    feat(connectors): create data normalizer
    Signed-off-by: mateonunez <mateonunez95@gmail.com>

--- packages/connectors/src/shared/data/normalizer/connector.data-normalizer.interface.ts
+++ packages/connectors/src/shared/data/normalizer/connector.data-normalizer.interface.ts
@@ -1,3 +1,8 @@
 export interface IConnectorDataNormalizer<T, U> {
   normalize(data: T): U;
 }
+
+export type ConnectorDataNormalizerMapping<T, U> = {
+  [K in keyof U]: keyof T | ((source: T) => U[K]);
+};
+
