dc51337513226ae1f950dae109b15fcb8ecc3835
mateonunez <mateonunez95@gmail.com>
2025-01-07 10:54:28 +0100

    perf: improve testing environment
    Signed-off-by: mateonunez <mateonunez95@gmail.com>

--- packages/infrastructure/postgres/src/db.migrate.ts
+++ packages/infrastructure/postgres/src/db.migrate.ts
@@ -5,6 +5,10 @@ import { getPostgresClient, closePostgresConnection } from "./postgres.client";
 
 dotenv.config();
 
+if (process.env.NODE_ENV === "test") {
+  dotenv.config({ path: ".env.test" });
+}
+
 async function runMigration() {
   try {
     console.log("⏳ Running migrations...");
